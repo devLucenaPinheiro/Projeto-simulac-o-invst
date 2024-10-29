6247bac74c24e1087314e6ccf4d69b2c287d42ad
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-10-29 17:43:33 -0300

    Ajustando a main + lógica
    Fazer com que a main fique abaixo da header e criando a lógica básica do cálculo

---
+++ script.js
@@ -0,0 +1,7 @@
+inicial = parseFloat(prompt("Digite o valor inicial"))
+mensal = parseFloat(prompt("Digite o aporte mensal"))
+duraçao = parseFloat(prompt("Digite o periodo (em meses)"))
+
+
+
+    alert("O lucro será de: " + incial + (mensal * duração))
